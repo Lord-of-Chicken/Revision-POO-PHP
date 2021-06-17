23096de89d8c81ae1775850d0db652b908bac928
Lord-of-Chicken <77158261+Lord-of-Chicken@users.noreply.github.com>
2021-06-17 18:55:21 +0200

    Update introduction.php
    2eme ex

--- introduction.php
+++ introduction.php
@@ -3,13 +3,29 @@
 class Employe {
    public $nom;
    public $prenom;
-   public $age;
+   private $age;
 
    public function __construct($prenom, $nom, $age){
     $this->nom = $nom;
     $this->prenom = $prenom;
-    $this->age = $age;   
+    $this->setAge($age);   
    }
+
+   public function setAge($age){
+       if (is_int($age) && $age >= 1 && $age <= 120) {
+        $this->age = $age;
+    } else {
+            throw new Exception("L'age devrait etre entre 1 et 120");
+        }
+   }
+       
+
+
+
+   public function getAge(){
+    return $this->age;
+
+}
 
    public function presentation() {
     var_dump("Bonjour, je suis $this->prenom $this->nom et j'ai $this->age ans ");
@@ -17,8 +33,13 @@ class Employe {
     }
 }
 
-$employe1 = new Employe("Gael", "Layeux", 43);
+$employe1 = new Employe("Gael", "Layeux", 50);
 $employe2 = new Employe("Barnar", "Goddi", 50);
+
+//500 lignes de code
+$employe1->setAge("jj");
+
+//500 lignes de code
 
 
 $employe1->presentation(); 
